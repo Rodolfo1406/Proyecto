d4ea0c77cb43645c895ddbe574d4cb9492162512
danivargas12 <45149866+danivargas12@users.noreply.github.com>
2018-11-18 13:41:01 -0600

    sdc
    sdf

---
+++ Persona/Usuario.vb
@@ -0,0 +1,26 @@
+﻿
+Public Class Usuario
+
+    Inherits Persona
+
+    Private _Usuario As String
+    Private _Contrasena As String
+
+    Public Property Usuario As String
+        Get
+            Return Usuario
+        End Get
+        Set(value As String)
+            _Usuario = value
+        End Set
+    End Property
+
+    Public Property Contrasena As String
+        Get
+            Return Contrasena
+        End Get
+        Set(value As String)
+            _Contrasena = value
+        End Set
+    End Property
+End Class
